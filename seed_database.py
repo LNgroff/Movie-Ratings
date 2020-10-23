91d29e292163eb0fe82fdebb461a5486877beb2a
dianasch <dianaschao@gmail.com>
2020-10-23 19:33:35 +0000

    Creates seed_database. This file reades the json file for movies and adds all movies as instances of the Movie class into our SQL database.
    Co-authored-by: LNgroff <lngroff@gmail.com>

---
+++ seed_database.py
@@ -0,0 +1,25 @@
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+
+import model
+import crud
+import server
+
+os.system('dropdb ratings')
+os.system('createdb ratings')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open('data/movies.json') as f:
+    movie_data = json.loads(f.read())
+
+
+movies_in_db = []
+for movie in movie_data:
+
+    db_movie = crud.create_movie(movie['title'], movie['overview'], movie['release_date'], movie['poster_path'])
+    
+    movies_in_db.append(db_movie)
